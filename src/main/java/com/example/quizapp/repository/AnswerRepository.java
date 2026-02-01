eae04efe12a1b9d4f64ab2014cced6003f1263b4
123santhosh2999 <rsanthosh2999@gmail.com>
2026-02-01 16:38:29 +0530

    Update AnswerRepository.java

--- src/main/java/com/example/quizapp/repository/AnswerRepository.java
+++ src/main/java/com/example/quizapp/repository/AnswerRepository.java
@@ -4,10 +4,12 @@ import com.example.quizapp.entity.Answer;
 import com.example.quizapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     Optional<Answer> findByUser(User user);
 
+    List<Answer> findByQuestionId(Long questionId);
 }
