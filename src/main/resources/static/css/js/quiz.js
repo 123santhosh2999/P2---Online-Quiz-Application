7a85b253d4746c425427cdb0bdc741dddae9bb65
123santhosh2999 <rsanthosh2999@gmail.com>
2026-02-01 16:13:14 +0530

    Update quiz.js

--- src/main/resources/static/css/js/quiz.js
+++ src/main/resources/static/css/js/quiz.js
@@ -1,4 +1,11 @@
 // Quiz Application JavaScript
+const payload = {
+  firstName: firstName.trim(),
+  lastName: lastName.trim(),
+  username: username.trim(),
+  email: email.trim(),
+  password
+};
 
 // Global variables
 let currentQuestion = 0;
